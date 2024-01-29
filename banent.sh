6d9e5ad1eb897e293a183c8a0f278cea92ab9cf3
Roman Gnedin <rgnedin@gmail.com>
2024-01-29 18:35:48 +0300

    script can use wrapper from first arg and the others are used for indexes

--- banent.sh
+++ banent.sh
@@ -1,10 +1,24 @@
-#! /bin/ash
+#!/bin/bash
 
-BAN='1'
-COMMIT=''
-ROLE=$(basename $0)
-if [ "$ROLE" = "unbanent" ]; then
-  BAN='0'
-  COMMIT=' && uci commit'
+REMOTE=$1
+if [ "z${REMOTE}z" = "zz" ]; then
+  REMOTE='echo '
+else
+  shift
 fi
-firewall.@rule[0].enabled="$BAN" $COMMIT && fw3 reload
+
+CONFS=$*
+if [ "z${CONFS}z" = "zz" ]; then
+  CONFS='0'
+fi
+REMOTE_CMD=''
+for CONF in $CONFS; do
+  BAN="uci set firewall.@rule[$CONF].enabled=1"
+  ROLE=$(basename $0)
+  if [ "$ROLE" = "unbanent" ]; then
+    BAN="uci revert firewall.@rule[$CONF]"
+  fi
+  REMOTE_CMD="$REMOTE_CMD $BAN &&"
+done
+REMOTE_CMD="$REMOTE_CMD fw3 reload"
+$REMOTE $REMOTE_CMD
