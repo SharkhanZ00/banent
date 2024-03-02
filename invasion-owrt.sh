01557905ead8e88736aeb3f5d3032dfc8ed95acd
Roman Gnedin <rgnedin@gmail.com>
2024-03-02 21:33:33 +0300

    invasion can do update execs even if user exists

--- invasion-owrt.sh
+++ invasion-owrt.sh
@@ -8,7 +8,7 @@ TAR=$(base64 -w0 ./banent.tgz)
 SSH_PK=$(cat $HOME/.ssh/id_rsa.pub | base64 -w0)
 
 SCRIPT="\
-/bin/ash -c 'useradd -N -r -m hass-rpc -s /bin/ash && \
+/bin/ash -c 'cat /etc/passwd | grep hass-rpc > /dev/null || useradd -N -r -m hass-rpc -s /bin/ash && \
 echo $TAR | base64 -d | tar -vzx -C / && \
 mkdir -p /overlay/upper/home/hass-rpc/.ssh && echo '$SSH_PK' | base64 -d >> /overlay/upper/home/hass-rpc/.ssh/authorized_keys && \
 chown -R hass-rpc:users /overlay/upper/home/hass-rpc/.ssh/authorized_keys && chmod 0600 /overlay/upper/home/hass-rpc/.ssh/authorized_keys && chmod 0700 /overlay/upper/home/hass-rpc/.ssh'"
