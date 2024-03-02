49505747dbb3a0d9283490cd57bf27815f266a15
Roman Gnedin <rgnedin@gmail.com>
2024-03-02 12:15:36 +0300

    from caller local banner script removed remote commands

--- banent-impl.sh
+++ banent-impl.sh
@@ -3,21 +3,11 @@
 BAN_TEMP='uci set firewall.@rule[$CONF].enabled=1'
 UNBAN_TEMP='uci revert firewall.@rule[$CONF]'
 
-REMOTE=$1
-echo "$REMOTE" | grep -E '^[0-9]+$' > /dev/null
-IS_DIGIT=$?
-if [ "z${REMOTE}z" = "zz" -o "$IS_DIGIT" = '0' ]; then
-  REMOTE='echo '
-  BAN_TEMP='sudo banent $CONF'
-  UNBAN_TEMP='sudo unbanent $CONF'
-else
-  shift
-fi
-
 CONFS=$*
 if [ "z${CONFS}z" = "zz" ]; then
   CONFS='0'
 fi
+
 REMOTE_CMD=''
 for CONF in $CONFS; do
   BAN=eval echo $BAN_TEMP
