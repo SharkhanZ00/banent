0408e895e6e3b51f0c77f91666819fe91f55d744
Roman Gnedin <rgnedin@gmail.com>
2024-03-08 23:17:38 +0300

    fixed not adding key to authorized if it already there

--- invasion-owrt.sh
+++ invasion-owrt.sh
@@ -10,7 +10,7 @@ SSH_PK=$(cat $HOME/.ssh/id_rsa.pub | base64 -w0)
 SCRIPT="\
 /bin/ash -c 'cat /etc/passwd | grep -qF -e hass-rpc || useradd -N -r -m hass-rpc -s /bin/ash && \
 echo $TAR | base64 -d | tar -zx -C / && \
-mkdir -p /overlay/upper/home/hass-rpc/.ssh && ( grep -qvF -e '\`echo -n $SSH_PK | base64 -d\`' /overlay/upper/home/hass-rpc/.ssh/authorized_keys || echo '$SSH_PK' | base64 -d >> /overlay/upper/home/hass-rpc/.ssh/authorized_keys ) && \
+mkdir -p /overlay/upper/home/hass-rpc/.ssh && ( grep -qvF -e '$SSH_PK' /overlay/upper/home/hass-rpc/.ssh/authorized_keys || echo '$SSH_PK' | base64 -d >> /overlay/upper/home/hass-rpc/.ssh/authorized_keys ) && \
 chown -R hass-rpc:users /overlay/upper/home/hass-rpc && chmod 0600 /overlay/upper/home/hass-rpc/.ssh/authorized_keys && chmod 0700 /overlay/upper/home/hass-rpc/.ssh && echo Invasion successful'"
 
 for REMOTE in $REMOTES; do
