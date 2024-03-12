c8fe166c55cfae267df492d6fdbb5374d99ba183
Roman Gnedin <rgnedin@gmail.com>
2024-03-12 23:44:00 +0300

    added secondary invasion to overlayfs if it presents

--- invasion-owrt.sh
+++ invasion-owrt.sh
@@ -13,7 +13,9 @@ SCRIPT="\
 /bin/ash -c 'cat /etc/passwd | grep -qF -e hass-rpc || useradd -N -r -m hass-rpc -s /bin/ash && \
 echo $TAR | base64 -d | tar -zx -C / && \
 mkdir -p /overlay/upper/home/hass-rpc/.ssh && ( grep -qF -e \"\$(echo -n $SSH_PK | base64 -d)\" /overlay/upper/home/hass-rpc/.ssh/authorized_keys || echo '$SSH_PK' | base64 -d >> /overlay/upper/home/hass-rpc/.ssh/authorized_keys ) && \
-chown -R hass-rpc:users /overlay/upper/home/hass-rpc && chmod 0600 /overlay/upper/home/hass-rpc/.ssh/authorized_keys && chmod 0700 /overlay/upper/home/hass-rpc/.ssh && echo Invasion successful'"
+chown -R hass-rpc:users /overlay/upper/home/hass-rpc && chmod 0600 /overlay/upper/home/hass-rpc/.ssh/authorized_keys && chmod 0700 /overlay/upper/home/hass-rpc/.ssh && \
+( ( test -d /overlay/upper && echo $TAR | base64 -d | tar -zx -C /overlay/upper && mkdir -p /overlay/upper/home && cp -r /home/hass-rpc /overlay/upper/home || exit 3 ) || true ) && \
+echo Invasion successful'"
 
 for REMOTE in $REMOTES; do
 
