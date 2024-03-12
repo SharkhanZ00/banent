8f6937db487303048f288c10aadc4b96b51fd379
Roman Gnedin <rgnedin@gmail.com>
2024-03-12 19:25:56 +0300

    caller has adapted for ash

--- banent-impl.sh
+++ banent-impl.sh
@@ -9,13 +9,13 @@ if [ "z${CONFS}z" = "zz" ]; then
 fi
 
 LOCAL_CMD=''
+ROLE=$(basename $0)
 for CONF in $CONFS; do
   BAN=$(eval echo -n $BAN_TEMP)
-  ROLE=$(basename $0)
   if [ "$ROLE" = "unbanent" ]; then
     BAN=$(eval echo -n $UNBAN_TEMP)
   fi
-  LOCAL_CMD="$LOCAL_CMD $BAN"
+  LOCAL_CMD="$LOCAL_CMD $BAN &&"
 done
-$LOCAL_CMD
-fw3 reload
+LOCAL_CMD="$LOCAL_CMD fw3 reload"
+/bin/ash -c "$LOCAL_CMD"
